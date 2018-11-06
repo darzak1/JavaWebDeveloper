53a1a73e712d5e0eaf0b2457ad2e2de4563e8569
darzak1 <darzak@gmail.com>
2018-11-06 09:29:39 +0100

    zadanie domowe

--- src/DomCwiczenia.java
+++ src/DomCwiczenia.java
@@ -6,14 +6,28 @@ public class DomCwiczenia {
         do {
             System.out.println("i=" + i);
             i++;
-        } while (i <= 100);
+        } while (i <= 4);
 
         double d = 3.1424234;
-
+        System.out.println(d);
         do {
             System.out.println("double= " + d);
             d--;
-        } while (d >= -14);
+        } while (d >= -4);
+
+
+        //int p = -24;
+        boolean pDodatnie = true;
+
+        //System.out.println("to jest p: " + p);
+        System.out.println("p jest dodatnie: " + pDodatnie);
+        for (int p = -4; p < 1; p--) {
+            System.out.println("to jest p: " + p);
+            if (p> -14);{
+                System.out.println("koniec :" +p);
+            }
+        }
 
     }
+
 }
